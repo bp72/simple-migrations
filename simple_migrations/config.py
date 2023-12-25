b9ce9dbc8e5462c51a8f340706fcad2cc8099c1c
Pavel Bitiukov <pavleg.bityukov@gmail.com>
2023-12-25 11:23:39 +0000

    [feature] migrations dir creation

--- simple_migrations/config.py
+++ simple_migrations/config.py
@@ -9,12 +9,14 @@ class Config:
             database_name: str = "simple",
             database_user: str = "simple",
             database_password: str = "simple",
+            migrations_dir: str = "migrations",
     ) -> None:
         self.database_host = database_host
         self.database_port = database_port
         self.database_name = database_name
         self.database_user = database_user
         self.database_password = database_password
+        self.migrations_dir = migrations_dir
 
     @property
     def connection_string(self) -> str:
--- simple_migrations/setup.py
+++ simple_migrations/setup.py
@@ -1,10 +1,13 @@
+import os
+import sys
+
 from simple_migrations.config import Config
 import psycopg
 
 
 def initial_setup(
 
-) -> None:
+) -> int:
     config = Config()
     with psycopg.connect(config.connection_string) as conn:
         with conn.cursor() as cur:
@@ -15,3 +18,13 @@ def initial_setup(
                     created_at timestamptz
                 )
             """)
+
+    sys.stdout.write("Created simple_migrations table\n")
+
+    if os.path.exists(config.migrations_dir) and os.listdir(config.migrations_dir):
+        sys.stderr.write(f"{config.migrations_dir} directory exists and not empty")
+        return 1
+
+    os.mkdir(config.migrations_dir)
+    sys.stdout.write(f"Created {config.migrations_dir} directory\n")
+    return 0
